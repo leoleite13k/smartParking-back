7c20cf2aaa153a4e8dc2d951b7d64afb8f0dabf5
Leonardo Leite <30642674+leoleite13k@users.noreply.github.com>
2019-05-31 21:39:29 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
   uri:
-    'mongodb+srv://admin:admin13@cluster0-5yui6.mongodb.net/SmartParking?retryWrites=true'
+    'mongodb+srv://yourUser:yourPassword@cluster0-5yui6.mongodb.net/SmartParking?retryWrites=true'
 }
